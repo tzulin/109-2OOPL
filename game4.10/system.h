ced43d75f573baff95cf062d0a9f9e47e5e1e065
astommotsa <aaa710.3@gmail.com>
2021-05-08 19:06:55 +0800

    meet method2

--- game4.10/system.h
+++ game4.10/system.h
@@ -5,47 +5,47 @@
 #include "./things.h"
 
 namespace game_framework {
+
 	template <class T, class U>
 	bool Meet(T a, U b) {
 		int* aXy = a.GetXy();
 		int* bXy = b.GetXy();
-
-		// a在b的左邊，有碰到
-		if (aXy[2] > bXy[0] && aXy[2] < bXy[2] && bXy[0] > aXy[0]) {
-			// a在b的上面，有碰到
-			if (aXy[3] > bXy[1] && aXy[3] < bXy[3] && bXy[1] > aXy[1]) {
-				delete[] aXy;
-				delete[] bXy;
-				return true;
+		
+		int i = 0, n = 1;
+		for (int count = 0; count < 2; count++) {
+			for (int _count = 0; _count < 2; _count++) {
+				if (aXy[i] > bXy[0] && aXy[i] < bXy[2] && aXy[n] > bXy[1] && aXy[n] < bXy[3]) {
+					delete[] aXy;
+					delete[] bXy;
+					return true;
+				}
+				n += 2;
 			}
-			// a在b的下面，有碰到
-			else if (aXy[1] > bXy[1] && aXy[1] < bXy[3] && bXy[3] < aXy[3]) {
-				delete[] aXy;
-				delete[] bXy;
-				return true;
-			}
-		}
-		// a在b的右邊，有碰到
-		else if (aXy[0] > bXy[0] && aXy[0] < bXy[2] && aXy[2] > bXy[2]) {
-			// a在b的上面，有碰到
-			if (aXy[3] > bXy[1] && aXy[3] < bXy[3] && bXy[1] > aXy[1]) {
-				delete[] aXy;
-				delete[] bXy;
-				return true;
-			}
-			// a在b的下面，有碰到
-			else if (aXy[1] > bXy[1] && aXy[1] < bXy[3] && bXy[3] < aXy[3]) {
-				delete[] aXy;
-				delete[] bXy;
-				return true;
-			}
+			n = 1;
+			i += 2;
 		}
 
+		i = 0, n = 1;
+		for (int count = 0; count < 2; count++) {
+			for (int _count = 0; _count < 2; _count++) {
+				if (bXy[i] > aXy[0] && bXy[i] < aXy[2] && bXy[n] > aXy[1] && bXy[n] < aXy[3]) {
+					delete[] aXy;
+					delete[] bXy;
+					return true;
+				}
+				n += 2;
+			}
+			n = 1;
+			i += 2;
+		}
+		
+		
 		// 沒碰到
 		delete[] aXy;
 		delete[] bXy;
  		return false;
 	}
+
 }
 
 
