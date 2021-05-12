7ef7ef005ff7e4cd2b78209761c833ae0598db65
tzulin <tzulin0517@gmail.com>
2021-05-12 18:08:03 +0800

    kirby IsEaten walk half finished

--- tzulin/game4.10/kirby.h
+++ tzulin/game4.10/kirby.h
@@ -49,8 +49,12 @@ namespace game_framework {
 	private:
 		CAnimation KirbyMovingL;
 		CAnimation KirbyMovingR;
+		CAnimation KirbyFatMovingL;
+		CAnimation KirbyFatMovingR;
 		CAnimation KirbyStand;
 		CAnimation KirbyStandL;
+		CAnimation KirbyFatStand;
+		CAnimation KirbyFatStandL;
 		CAnimation KirbyJumpR;
 		CAnimation KirbyJumpL;
 		CAnimation KirbyScreamR;
